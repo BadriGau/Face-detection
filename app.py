e56a9d1b2c5eefce6cf350ab71fef69cb6194078
Badri <badrigautam19@gmail.com>
2021-08-14 13:38:06 +0200

    read camera and show video feed

--- app.py
+++ app.py
@@ -8,6 +8,23 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+camera = cv2.VideoCapture(0) 
+
+def gen_frames():  
+    while True:
+        success, frame = camera.read() 
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n') 
+
 
 
 if __name__ == "__main__":
